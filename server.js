5e4831d7ecca6f0dbd8aa4ba0237bd90e3702dd6
Heggy Castaneda <heggyy@gmail.com>
2021-06-04 15:53:18 -0400

    done with post method

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ const express = require("express");
 const cors = require('cors');
 const es6Renderer = require('express-es6-template-engine');
 
+// name export v4 but rename it uuidv4
+const { v4: uuidv4 } = require('uuid');
+
 const app = express();
 
 // when you see app.use - it is middleware, specify static directory
 app.use(cors());
 app.use(express.static('public'));
+// middleare for body parsing
+app.use(express.json());
+// for parsing application/x-www-form-urlencoded (converts str => json)
+// app.use(express.urlencoded({ extended: true })) 
 
 // Configure Template Engine
 app.engine('html', es6Renderer);
@@ -16,6 +23,26 @@ app.set('view engine', 'html');
 
 const data = require('./dataObject')
 // console.log(data)
+
+// 6. Post Method to handle new user
+app.post("/profile", (req, res) => {
+    console.log('full req.body: =====>', req.body);
+
+    // Create ID
+    const id = uuidv4();
+
+    // setting keys
+    req.body.id = id;
+    req.body.images = [];
+    // Save data server memory only exists during server is on
+    data[id] = req.body;
+    console.log('after ___full data transform: ===>', data);
+    console.log('after ___full id transform: ===>', id);
+    console.log('after ___full data[id] transform: ===>', data[id]);
+    console.log('after ___full req.body transform: ===>', req.body);
+    res.status(200).send()
+
+});
 
 // 4. Detail page here. 
 app.get("/profile/:id", (req, res) => {
@@ -56,5 +83,5 @@ app.get("*", (req, res)=>{
 
 // 2. Start your express server
 app.listen(3030, ()=>{
-    console.log("running on port 3030")
+    console.log("running on port http://localhost:3030")
 })
